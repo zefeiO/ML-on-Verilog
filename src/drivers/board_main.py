8d67383d15f770ab4167e5f7d11e72c7cc0fdccb
Zefei <ouzefei1551@gmail.com>
2025-01-18 01:53:30 -0500

    board server coroutines

--- src/drivers/board_main.py
+++ src/drivers/board_main.py
@@ -5,9 +5,13 @@ import struct
 import pickle
 import logging
 import asyncio
+import random
+from asyncio import Queue, StreamReader, StreamWriter
 import numpy as np
 from common import Message
 
+RECONNECT_DELAY_INITIAL = 1
+RECONNECT_DELAY_MAX = 32
 
 class BoardServer:
     class BoardStates(Enum):
@@ -17,55 +21,146 @@ class BoardServer:
         UNKNOWN = 4
     state = BoardStates.START
 
-    def run(self, board_addr):
+    def __init__(self, board_host, board_port, receiver_host, receiver_port):
+        self.host = board_host
+        self.port = board_port
+        self.receiver_host = receiver_host
+        self.receiver_port = receiver_port
+        self.job_queue: Queue[np.ndarray] = Queue()
+        self.result_queue: Queue[np.ndarray] = Queue()
+
+    async def start(self, board_host, board_port):
+        receiver: asyncio.Server = await asyncio.start_server(self.co_recv, self.host, self.port)
+        addr = receiver.sockets[0].getsockname()
+        print(f"[Info] Receiver service started on {addr}")
+
+        asyncio.create_task(self.co_infer)
+        asyncio.create_task(self.co_send)
+
+        async with receiver:
+            await receiver.serve_forever()
+
+    # Producer of job_queue
+    async def co_recv(self, reader: StreamReader, writer: StreamWriter):
+        addr = writer.get_extra_info('peername')
+        print(f"[Info] Receiver connected to {addr}")
+
+        try:
+            while True:
+                #################################
+                # Message packet format
+                #   length: 4 bytes (length of Message object)
+                #   Message object (type, data)
+                ##################################
+                length_field = reader.read(4)
+                if not length_field:
+                    print(f"[Info] Connection closed by {addr}")
+                    break
+                msg_len: int = struct.unpack("!I", length_field)[0] # big-endian uint32
+                msg_buf = reader.read(msg_len)
+                if not msg_buf:
+                    print(f"[Info] Connection closed by {addr}")
+                    break
+                msg: Message = pickle.loads(msg_buf)
+                
+                # Handle message
+                if msg.message_type == "model":
+                    self.deploy_model()
+                    self.state = self.BoardStates.READY
+                elif msg.message_type == "input":
+                    if self.state == self.BoardStates.START:
+                        # don't change state here
+                        print(f"[ERROR] received input data at state START")
+                    else: # (READY, RUNNING) -> RUNNING
+                        self.state = self.BoardStates.RUNNING 
+                        try:
+                            await self.job_queue.put(msg.data)
+                        except asyncio.QueueFull:
+                            pass # TODO: handle QueueFull exception
+                else:
+                    raise Exception(f"Unexpected message type: {msg.message_type}")
+        except asyncio.IncompleteReadError:
+            print(f"[Error] Connection lost from {addr}")
+        finally:
+            writer.close()
+            await writer.wait_closed()
+            print(f"[Info] Connection with {addr} closed.")
+
+
+    # Consumer of job_queue, Producer of result_queue
+    # Execute inference job on FPGA one at a time and send result to downstream receiver
+    async def co_infer(self):
         while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(board_addr)
-                s.listen(1)
-                print(f"[INFO] Waiting for a connection on {board_addr}...")
+            inputs = await self.job_queue.get()
+            outputs = await self.run_model(inputs)
+            await self.result_queue.put(outputs)
 
-                conn, pc_addr = s.accept()
-                while conn:
-                    msg = self.receive_message(conn)
-                    self.handle_message(msg)
 
-    # TODO: consider making this a coroutine to increase concurrency
-    def receive_message(self, conn: socket.socket) -> Message:
-        #################################
-        # Message packet format
-        #   length: 4 bytes (length of Message object)
-        #   Message object (type, data)
-        ##################################
-        length_field = conn.recv(4)
-        msg_len: int = struct.unpack("!I", length_field)[0] # big-endian uint32
-        msg_buf = conn.recv(msg_len)
-        return pickle.loads(msg_buf)
+    # Consumer of result_queue
+    async def co_send(self):
+        async def connect_receiver():
+            try:
+                _, writer = await asyncio.open_connection(self.receiver_host, self.receiver_port)
+                return writer
+            except (ConnectionRefusedError, asyncio.TimeoutError) as e:
+                print(f"[Error] Connection failed: {e}")
+            except Exception as e:
+                print(f"[Error] Unexpected error during connection: {e}")
+            return None
 
-    # TODO: consider making this a coroutine to increase concurrency
-    def handle_message(self, msg: Message, job_queue: asyncio.Queue):
-        if msg.message_type == "model":
-            self.deploy_model()
-            self.state = self.BoardStates.READY
-        elif msg.message_type == "input":
-            if self.state == self.BoardStates.START:
-                # don't change state here
-                print(f"[ERROR] received input data at state START")
-            else: # (READY, RUNNING) -> RUNNING
-                self.state = self.BoardStates.RUNNING 
-                try:
-                    job_queue.put_nowait(msg.data)
-                except asyncio.QueueFull:
-                    pass # TODO: handle QueueFull exception
-        else:
-            raise Exception(f"Unexpected message type: {msg.message_type}")
+        attempt = 0
+        def get_exponential_backoff() -> float:
+            """Calculates the delay before the next reconnection attempt."""
+            nonlocal attempt
+            delay = min(RECONNECT_DELAY_INITIAL * 2 ** (attempt - 1), RECONNECT_DELAY_MAX)
+            # Adding jitter to prevent thundering herd problem
+            jitter = random.uniform(0, 1)
+            return delay + jitter
         
+        writer: StreamWriter = None
+        while True:
+            if writer is None or writer.is_closing():
+                attempt += 1
+                delay = get_exponential_backoff()
+                print(f'Attempting to reconnect in {delay:.2f} seconds...')
+                await asyncio.sleep(delay)
+                writer = await connect_receiver()
+                if writer is None:
+                    continue
+                attempt = 0
+
+            outputs = await self.result_queue.get()
+            if outputs is None:
+                print("[Info] co_send exiting...")
+                break
+            msg = Message("input", outputs)
+            msg_buf = pickle.dumps(msg)
+            msg_buf = struct.pack("!I", len(msg_buf)) + msg_buf
+
+            try:
+                writer.write(msg_buf)
+                await writer.drain()
+            except (ConnectionResetError, BrokenPipeError) as e:
+                print(f'Connection lost: {e}')
+                writer.close()
+                await writer.wait_closed()
+                writer = None
+                await self.result_queue.put(outputs)
+            except Exception as e:
+                print(f'Unexpected error in sender: {e}')
+                writer.close()
+                await writer.wait_closed()
+                writer = None
+                await self.result_queue.put(outputs)
+
+
     def deploy_model(self, data: bytes):
         # TODO: implementation
         pass
 
-    # Consumer of job_queue
-    def inferece_coroutine(self, job_queue: asyncio.Queue, receiver_addr):
-        # TODO: execute inference job on FPGA one at a time and send result to downstream receiver
+
+    async def run_model(self, inputs: list[np.ndarray]) -> list[np.ndarray]:
+        # TODO: implementation
         # check how to wrap pynq calls in a coroutine
         # should be doable theoretically because computation is done on FPGA rather than CPU
         pass
